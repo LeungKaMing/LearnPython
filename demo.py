49bc7a9f39bfb1c8642e5d0872fef1fb7e11ef67
leungkaming <510314588@qq.com>
2018-08-28 22:08:54 +0800

    feat: :wrench:0828增加运算检查

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
-userInput = input('请输入您的姓名：')
+userInput = input('请输入：')
 if userInput == 'admin':
   print('欢迎您，管理员！')
+elif userInput == 'ljm':
+  print('欢迎您，' + userInput)
+elif userInput == 'cal':
+  x = input('请输入第一位操作数：')
+  y = input('请输入第二位操作数：')
+  method = input('请输入运算法则：')
+  sum = int(x) + int(y)
+  print('x' + method + ' y = ' + str(sum))
+else:
+  print('kidding me?您输入的是：' + userInput + '字符串长度是：' + str(len(userInput)))
