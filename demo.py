78d2a885e5a8ec37b884502d1b9fd552599269c1
LeungKaMing <510314588@qq.com>
2018-08-29 01:34:16 +0800

    feat: :wrench:0829写了一个简单程序：能实现简单输出字符串；四则运算；简单电话本信息录入

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+#-*- coding:UTF-8 -*
 userInput = input('请输入：')
 if userInput == 'admin':
   print('欢迎您，管理员！')
@@ -7,7 +9,56 @@ elif userInput == 'cal':
   x = input('请输入第一位操作数：')
   y = input('请输入第二位操作数：')
   method = input('请输入运算法则：')
-  sum = int(x) + int(y)
-  print('x' + method + ' y = ' + str(sum))
+  if method == '+':
+    sum = int(x) + int(y)
+  elif method == '-':
+    sum = int(x) - int(y)
+  elif method == '*':
+    sum = int(x) * int(y)
+  elif method == '/':
+    sum = int(x) / int(y)
+  elif method == '%':
+    sum = int(x) % int(y)
+  print('sum: ', sum)
+elif userInput == 'list':
+  list = ['Michael', 'Jack', 'Bob', 'Tracy', 'Adam']
+  list.append('Skr')
+  bobIndex = 2
+  nameInput = input('Enter ur name, then u will line up before Bob...')
+  list.insert(bobIndex, nameInput)
+  print('Now the line becomes: ', list)
+  print('Everybody hates Adam，so I will delete him from the list')
+  list.pop()
+  print('Now the line becomes: ', list)
+  print('I hate Bob I will delete him from the list')
+  list.remove('Bob')
+  print('Now the line becomes: ', list)
+elif userInput == 'addrbook':
+  addrList = []
+  userName = input('U can enter ur name：')
+  userAge = input('U can enter ur age：')
+  userAddr = input('U can enter ur addr：')
+  userTel = input('U can enter ur tel：')
+  addrList.append({
+    'userName': userName,
+    'userAge': userAge,
+    'userAddr': userAddr,
+    'userTel': userTel
+  })
+  count = 1
+  while count <= 2:
+    # 先最多录入5个人的信息
+    userName = input('U can enter ur name in {0} time：'.format(count + 1))
+    userAge = input('U can enter ur age in {0} time：'.format(count + 1))
+    userAddr = input('U can enter ur addr in {0} time：'.format(count + 1))
+    userTel = input('U can enter ur tel in {0} time：'.format(count + 1))
+    addrList.append({
+      'userName': userInput,
+      'userAge': userAge,
+      'userAddr': userAddr,
+      'userTel': userTel
+    })
+    count = count + 1
+  print('Ur addrbook becomes this: ', addrList)
 else:
-  print('kidding me?您输入的是：' + userInput + '字符串长度是：' + str(len(userInput)))
+  print('尊敬的用户您好，您所输入的{0}并不匹配条件，输入字符串的长度为{1}'.format(userInput, len(userInput)))
